89c3cb7f9e223e87962d3f57397537920da4beab
jiyunsisgod <114905530+jiyunsisgod@users.noreply.github.com>
2022-12-29 16:53:15 +0900

    백준 10171번 문제 해결

---
+++ src/no_10171.java
@@ -0,0 +1,9 @@
+public class no_10171 {
+  public static void main(String[] args) throws Exception {
+    System.out.println("\\    /\\");
+    System.out.println(" )  ( ')");
+    System.out.println("(  /  )");
+    System.out.println(" \\(__)|");
+    
+  }
+}
