30c48d664239d163fdd5100e33695c1490ee74f1
jiyunsisgod <114905530+jiyunsisgod@users.noreply.github.com>
2022-12-30 23:37:55 +0900

    백준 2525문제 해결

---
+++ src/no_2525.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class no_2525 {
+  public static void main(String[] args) throws Exception{
+    Scanner sc =  new Scanner(System.in);
+
+    int h,m,mm;
+    h = sc.nextInt();
+    m = sc.nextInt();
+    mm = sc.nextInt();
+  }
+}
