693bb89d29d5302a3d74a2d9e3733cd385cbf333
jiyunsisgod <114905530+jiyunsisgod@users.noreply.github.com>
2023-01-03 23:34:12 +0900

    백준 2480 문제 중간까지 풀이 ..!

---
+++ src/no_2480.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class no_2480 {
+  public static void main(String[] args) throws Exception{
+    Scanner sc =  new Scanner(System.in);
+    String input = sc.nextLine();
+    String[] abc = input.split(" ");
+    int a = Integer.parseInt(abc[0]);
+    int b = Integer.parseInt(abc[1]);
+    int c = Integer.parseInt(abc[2]);
+
+    if(a == )
+  }
+}
