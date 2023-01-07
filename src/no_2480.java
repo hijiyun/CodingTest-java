ff835226c742540b425b483a8d0e632aa8f945c6
jiyunsisgod <114905530+jiyunsisgod@users.noreply.github.com>
2023-01-07 22:48:34 +0900

    백준 2480 문제 풀이

--- src/no_2480.java
+++ src/no_2480.java
@@ -9,6 +9,17 @@ public class no_2480 {
     int b = Integer.parseInt(abc[1]);
     int c = Integer.parseInt(abc[2]);
 
-    if(a == )
+    if(a == b && b == c && a == c)
+    {
+      System.out.println(10000+(a*1000));
+    }
+    else if (a == b || a == c){
+      System.out.println(1000+(a*100));
+    } 
+    else if (b==c){
+      System.out.println(1000+(b*100));
+    }
+    else 
+      System.out.println(Math.max(a, Math.max(b, c))*100);
   }
 }
