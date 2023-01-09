58eb18202e75ad0164336ea214dfbea0257b7435
jiyunsisgod <114905530+jiyunsisgod@users.noreply.github.com>
2023-01-09 19:21:14 +0900

    백준 2739문제풀이 완료

--- src/no_2739.java
+++ src/no_2739.java
@@ -3,5 +3,11 @@ import java.util.Scanner;
 public class no_2739 {
   public static void main(String[] args) throws Exception{
     Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    sc.close();
+
+    for(int i=1; i<10; i++){
+      System.out.println(n +" * "+i+" = " +(n*i));
+    }
   }
 }
