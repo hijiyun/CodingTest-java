a2a67f00019b1585c3e272f967557751f1e0626a
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-01-18 00:52:53 +0900

    15552 시간초과되서 다시BufferedReader써서 제출

---
+++ src/no_15552_2.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class no_15552_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int input = Integer.parseInt(br.readLine());
+        for (int i = 0; i < input; i++) {
+            String s = br.readLine();
+            int a = Integer.parseInt(s.split(" ")[0]);
+            int b = Integer.parseInt(s.split(" ")[1]);
+            bw.write(a + b + "\n");
+        }
+        bw.flush();
+    }
+}
